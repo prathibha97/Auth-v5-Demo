6eed84131919538b4597b0a980a03bba5e2bc0bc
Prathibha Ratnayake <prsthibha@gmail.com>
2024-01-21 12:54:25 +0530

    configure middleware and add sign in functionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,35 @@
 'use server';
+import { getUserByEmail } from '@/data/user';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
 
-export const register = async(values: z.infer<typeof RegisterSchema>) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: 'Invalid fields!' };
   }
 
-  return { success: 'Email sent!' };
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
+
+  return { success: 'User created!' };
 };

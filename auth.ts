688db176ef1bbbc5155ffb3e3011b15476c4e521
Prathibha Ratnayake <prsthibha@gmail.com>
2024-01-21 13:45:56 +0530

    add role based authentication with callbacks

--- auth.ts
+++ auth.ts
@@ -1,11 +1,44 @@
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import authConfig from '@/auth.config';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient, UserRole } from '@prisma/client';
+import NextAuth from 'next-auth';
+import { getUserById } from './data/user';
 
 const prisma = new PrismaClient();
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id!);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
+    // @ts-ignore
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   ...authConfig,

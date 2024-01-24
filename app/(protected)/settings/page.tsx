a85a172f473a13fe5b5b7c328e9bf9bd22531289
Prathibha Ratnayake <prsthibha@gmail.com>
2024-01-24 18:56:15 +0530

    update setting page to client component

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,19 @@
-import { auth, signOut } from '@/auth';
+'use client';
 
-const SettingsPage = async () => {
-  const session = await auth();
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { signOut } from 'next-auth/react';
+
+const SettingsPage = () => {
+  const user = useCurrentUser();
+
+  const onClick = () => {
+    signOut();
+  };
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <button type='submit'>Sign out</button>
-      </form>
+    <div className='bg-white p-10 rounded-xl'>
+      <button type='submit' onClick={onClick}>
+        Sign out
+      </button>
     </div>
   );
 };
---
+++ components/auth/user-button.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import LogoutButton from '@/components/auth/logout-button';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { ExitIcon } from '@radix-ui/react-icons';
+import { FC } from 'react';
+import { FaUser } from 'react-icons/fa';
+
+interface UserButtonProps {}
+
+const UserButton: FC<UserButtonProps> = ({}) => {
+  const user = useCurrentUser();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || undefined} />
+          <AvatarFallback className='bg-sky-500 text-white'>
+            <FaUser />
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-40' align='end'>
+        <LogoutButton>
+          <DropdownMenuItem>
+            <ExitIcon className='h-4 w-4 mr-2' />
+            Logout
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserButton;
